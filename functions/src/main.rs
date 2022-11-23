838d9c8dafbc9291bdee0d2b449374dfe6fded1b
Jorge Carlos <skay_1994@yahoo.com.br>
2022-11-23 15:15:45 -0300

    Feat: Added functions to some and check bigger number

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,16 @@
+fn dobro(num: i32) -> i32 {
+    return 2*num;
+}
+
+fn maior(a:i32, b:i32) -> i32 {
+    if a >= b {
+        return a;
+    } else {
+        return b;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("o dobro de 5 eh {}", dobro(5));
+    println!("o maior numero entre 5 e 4 eh {}", maior(5, 4));
 }
