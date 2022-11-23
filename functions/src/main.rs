51204afcf766a78dfe8b401f6a3063b7a8a7310a
Jorge Carlos <skay_1994@yahoo.com.br>
2022-11-23 15:16:12 -0300

    Feat: Test function implicity return

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -10,6 +10,12 @@ fn maior(a:i32, b:i32) -> i32 {
     }
 }
 
+fn alguma_fn(par_a: f32, par_b: i128) -> f32 {
+    println!("Essa funcao devolve um valor flutuante");
+    let x = 10.1f32 * par_a + par_b as f32;
+    x
+}
+
 fn main() {
     println!("o dobro de 5 eh {}", dobro(5));
     println!("o maior numero entre 5 e 4 eh {}", maior(5, 4));
