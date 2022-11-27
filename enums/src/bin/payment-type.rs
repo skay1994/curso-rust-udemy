596d53de8b7e75bef44b55e0e498dc73613376fe
Jorge Carlos <skay_1994@yahoo.com.br>
2022-11-27 12:23:50 -0300

    Feat: Add enum type values

--- enums/src/bin/payment-type.rs
+++ enums/src/bin/payment-type.rs
@@ -1,15 +1,16 @@
 enum Pagamento {
-    Dinheiro,
-    CreditoCartao,
-    DebitoCartao,
+    Dinheiro(f32),
+    CreditoCartao(bool, f32),
+    _DebitoCartao(bool, f32),
 }
 
 fn main() {
-    let pessoa_pagamento:Pagamento = Pagamento::DebitoCartao;
+    let pessoa_pagamento:Pagamento = Pagamento::CreditoCartao(false, 100f32);
 
     match pessoa_pagamento {
-        Pagamento::Dinheiro => println!("A pessoa pagou em Dinheiro!"),
-        Pagamento::CreditoCartao => println!("A pessoa pagou em Cartão de Credito!"),
+        Pagamento::Dinheiro(f) => println!("A pessoa pagou em Dinheiro! {}", f),
+        Pagamento::CreditoCartao(true, f) => println!("A pessoa pagou em Cartão de Credito! {}", f),
+        Pagamento::CreditoCartao(false, f) => println!("O pagamento em Cartão de credito não funciono, o valor {} não foi pago", f),
         _ => {}
     }
 }
