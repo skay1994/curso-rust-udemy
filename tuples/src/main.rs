c7480a56e03b121a6e2849b0daa3edeea0494c72
Jorge Carlos <skay_1994@yahoo.com.br>
2022-11-27 10:48:33 -0300

    Feat: Destructuring tuples and show

--- tuples/src/main.rs
+++ tuples/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
     let tupla = (12, "valores", 3.14, (1,2,3));
-    println!("{}", tupla.1);
-    println!("{}", (tupla.3).2);
+    let (a, b, c, d) = tupla;
+    println!("O valor de a eh {}", a);
+    println!("O valor de b eh {}", b);
+    println!("O valor de c eh {}", c);
+    println!("O valor de d eh {:?}", d);
 }
