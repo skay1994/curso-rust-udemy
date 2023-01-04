e5e4a04b68119fb70a6a0addcd731dbf5bf46c7a
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-04 15:42:12 -0300

    Feat: Create a array print specific index and print all with for and range

--- array/src/main.rs
+++ array/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let numeros_inteiros = [1,2,3,4,5];
+    println!("{}\n", numeros_inteiros[4]);
+
+    for i in 0..numeros_inteiros.len() {
+        println!("{}", numeros_inteiros[i]);
+    }
 }
