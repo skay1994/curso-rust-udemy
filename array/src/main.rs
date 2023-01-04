1e7d6ba4ef703c7edfcd814fa86ec1e7674f34a3
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-04 15:44:57 -0300

    Feat: Add type and size for array

--- array/src/main.rs
+++ array/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let numeros_inteiros = [1,2,3,4,5];
+    let numeros_inteiros: [i32; 5] = [1,2,3,4,5];
     println!("{}\n", numeros_inteiros[4]);
 
     for i in numeros_inteiros.iter() {
