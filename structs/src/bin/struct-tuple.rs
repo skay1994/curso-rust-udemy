60bb8060d51979c7edb7cedc56ac53bc688f1cf2
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-04 15:30:59 -0300

    Feat: Change variable to mutable

--- structs/src/bin/struct-tuple.rs
+++ structs/src/bin/struct-tuple.rs
@@ -1,15 +1,17 @@
 struct User (String, String, bool, String);
 
 fn main() {
-    let pessoa = User(
+    let mut pessoa = User(
         String::from("JoaoPessoa"),
         String::from("joaopessoa@gmail.com"),
         true,
         String::from("Homem")
     );
 
+    pessoa.0 = String::from("JoaoPessoa123");
+
     println!(
-            "O nome do usuario eh {}, seu email eh {}. A conta está ativa? {}",
+        "O nome do usuario eh {}, seu email eh {}. A conta está ativa? {}",
         pessoa.0,
         pessoa.1,
         pessoa.2
