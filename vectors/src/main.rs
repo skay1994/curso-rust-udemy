34a8c727beb276363f8427f1e6a6a13b0747bc03
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-04 16:21:28 -0300

    Feat: Add and show vectors values

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let mut vetores = vec![1,2,3,4];
+    let mut vetores2: Vec<i32> = Vec::new();
+
+    println!("{}", vetores[0]);
+
+    vetores.push(5);
+
+    println!("{}", vetores[4]);
+
+    vetores.remove(1);
+
+    println!("{:?}", vetores);
+
+    for i in vetores.iter() {
+        println!("{}",i);
+    }
 }
