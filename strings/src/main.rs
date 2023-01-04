721d0bb2168067c53113431f4a5874f039b6a2cf
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-03 23:18:54 -0300

    Feat: Make a for in values by split_whitespace

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
-    let mut minhaString: String = String::from("Ola meu nome é Jorge. ");
+    let minhaString: String = String::from("Ola meu nome é Jorge. ");
     println!("o tamanho dessa string eh {}", minhaString.len());
     println!("A minha string está vazia? {}", minhaString.is_empty());
+
+    for token in minhaString.split_whitespace() {
+        println!("{}", token);
+    }
 }
