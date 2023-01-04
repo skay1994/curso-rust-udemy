f46aff722e0bf01eaf45330661f0c6d390bfc121
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-03 23:06:40 -0300

    Feat: Change pessoa variable to mut and change ativo value

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -7,17 +7,19 @@ struct User {
 }
 
 fn main() {
-    let pessoa = User {
+    let mut pessoa = User {
         username: String::from("JoaoPessoa"),
         email: String::from("joaopessoa@gmail.com"),
         ativo: true,
         genero: String::from("Homem"),
     };
+    
+    pessoa.ativo = false;
 
     println!(
         "O nome do usuario eh {}, seu email eh {} e seu genero eh {}",
         pessoa.username,
         pessoa.email,
         pessoa.genero
-    );
+    )
 }
