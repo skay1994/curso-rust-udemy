4c2c292e079e019651864a11c39b3a1c3cadd36b
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-03 23:18:16 -0300

    Feat: Create a string and show length and if is empty

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    let mut minhaString: String = String::from("Ola meu nome é Jorge. ");
+    println!("o tamanho dessa string eh {}", minhaString.len());
+    println!("A minha string está vazia? {}", minhaString.is_empty());
 }
