a0af6893f621e4b8fd0e71ad5cdea782833ad1fd
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-04 17:29:28 -0300

    Feat: Create a hashmap and show length

--- hashmaps/src/main.rs
+++ hashmaps/src/main.rs
@@ -1,3 +1,11 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut hashmap = HashMap::new();
+    hashmap.insert("Matematica", 90);
+    hashmap.insert("Portugues", 72);
+    hashmap.insert("Biologia", 58);
+    hashmap.insert("Informatica", 96);
+
+    println!("Quantas materias o aluno cursou? {}", hashmap.len());
 }
