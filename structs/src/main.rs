bc84d34edfa64ac57610a455e67915b7b3ba141c
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-03 23:05:52 -0300

    Feat: Create User struct and print content

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,23 @@
+#[derive(Debug)]
+struct User {
+    username: String,
+    email: String,
+    ativo: bool,
+    genero: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let pessoa = User {
+        username: String::from("JoaoPessoa"),
+        email: String::from("joaopessoa@gmail.com"),
+        ativo: true,
+        genero: String::from("Homem"),
+    };
+
+    println!(
+        "O nome do usuario eh {}, seu email eh {} e seu genero eh {}",
+        pessoa.username,
+        pessoa.email,
+        pessoa.genero
+    );
 }
