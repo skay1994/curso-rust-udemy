b9c95ca6913eedc464730b99c5c5a42b220a148f
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-05 13:42:27 -0300

    Feat: Add rand bool

--- rand_numbers/src/main.rs
+++ rand_numbers/src/main.rs
@@ -5,5 +5,8 @@ use rand::Rng;
 fn main() {
     let valores_randomicos = rand::thread_rng().gen_range(5..11);
 
-    println!("{}", valores_randomicos)
+    println!("{}", valores_randomicos);
+
+    let booleano_randomico = rand::thread_rng().gen_bool(1.0 / 10.0);
+    println!("O resulta do é: {}", booleano_randomico)
 }
