74d7dc23ec50e08673c89166b095a8e97457ea0c
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-05 12:57:07 -0300

    Feat: Print rand value by range

--- rand_numbers/src/main.rs
+++ rand_numbers/src/main.rs
@@ -1,3 +1,9 @@
+extern crate rand;
+
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let valores_randomicos = rand::thread_rng().gen_range(5..11);
+
+    println!("{}", valores_randomicos)
 }
