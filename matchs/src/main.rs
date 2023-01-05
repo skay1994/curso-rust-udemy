72a0b9603ded75ef3256dd2ea793d4bc4ac2ea14
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-04 23:06:47 -0300

    Feat: Create match expression

--- matchs/src/main.rs
+++ matchs/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let numero = 2;
+
+    match numero {
+        1 => println!("O numero é 1"),
+        2 => println!("O numero é 2"),
+        _ => println!("Eu nao sei que numero é"),
+    }
 }
