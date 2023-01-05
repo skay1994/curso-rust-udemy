8185f7c9e2ec81d5e10a483944524f940b52d4af
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-05 14:48:10 -0300

    Feat: Update struct access

--- modules/src/bin/pessoa_module.rs
+++ modules/src/bin/pessoa_module.rs
@@ -3,7 +3,7 @@ mod module;
 use module::structs::Pessoa;
 
 fn main() {
-    let pessoa = Pessoa::Pessoa {
+    let pessoa = Pessoa {
         nome: String::from("Jorge"),
         sobrenome: String::from("Carlos"),
         idade: 28,
